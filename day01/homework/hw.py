243f2e8e28a4dddd9a0295e03c6d888571486a6e
Mac Book Pro <macbookpro@MacBook-Pro-Mac.local>
2025-02-24 23:20:16 +0400

    commit

--- day01/homework/hw.py
+++ day01/homework/hw.py
@@ -2,6 +2,6 @@ name = "nika"
 lastname ="pkhakadze"
 age="11"
 hobby="fexburti"
-print("chemi saxelia",name,"chemi gvaria",lastname,"chemi asakia",age,"chemi hobia",hobby)
+print("chemi saxelia",name,"chemi gvaria",lastname,"chemi asakia",age,"chemi hobia",hobbyg)
 
 
