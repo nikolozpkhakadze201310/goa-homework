cc467944a0dab577b7a9dcb37fceac3d5de37f18
Mac Book Pro <macbookpro@MacBook-Pro-Mac.local>
2025-02-24 17:06:50 +0400

    commit

---
+++ day00/homework/hw.py
@@ -0,0 +1,1 @@
+#axa vwer davalebas
---
+++ day01/homework/hw.py
@@ -0,0 +1,7 @@
+name = "nika" 
+lastname ="pkhakadze"
+age="11"
+hobby="fexburti"
+print("chemi saxelia",name,"chemi gvaria",lastname,"chemi asakia",age,"chemi hobia",hobby)
+
+
